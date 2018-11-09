db06b372cbc0acf9d556fe610543662623049ebe
Jun Zeng <zengjunsjtu@berkeley.edu>
2018-11-08 22:22:22 -0800

    Added the code for animation and film

--- main.m
+++ main.m
@@ -36,18 +36,18 @@ end
 x_0 = [xL; vL; q; omega; reshape(R, 9,1); Omega];
 
 %% Solving Dynamical Equations
-odeopts = odeset('RelTol',1e-2,'AbsTol',1e-2);
+odeopts = odeset('RelTol',1e-1,'AbsTol',1e-1);
 % Don't forget to change the 2odel of control for the part of "Compute
 % various quantities" 
-[t, x] = ode45(@odefun_control2, [0 30], x_0, odeopts, data);
+[t, x] = ode45(@odefun_control1, [0 30], x_0, odeopts, data);
 
 
 %% Compute various quantities
 
 disp('Computing State Variables and Configuration Errors ') ;
-ind = round(linspace(1, length(t), round(0.1*length(t)))) ;
+ind = round(linspace(1, length(t), round(0.001*length(t)))) ;
 for j=ind
-    [~, xLd_, Rd, qd_, f_, M_] = odefun_control2(t(j), x(j,:)', data) ;
+    [~, xLd_, Rd, qd_, f_, M_] = odefun_control1(t(j), x(j,:)', data) ;
     [phi_d(j),theta_d(j),psi_d(j)] = RotToRPY_ZXY(Rd) ;
     [phi(j),theta(j),psi(j)] = RotToRPY_ZXY(reshape(x(j,13:21),3,3));
     xLd(j,:)=xLd_';
@@ -64,7 +64,7 @@ end
 
 %% Plotting various states/outputs/inputs
 disp('Computing Plotting Figures') ;
-ind = round(linspace(1, length(t), round(0.1*length(t)))) ;
+ind = round(linspace(1, length(t), round(0.001*length(t)))) ;
 
 % Input Moment
 figure;
